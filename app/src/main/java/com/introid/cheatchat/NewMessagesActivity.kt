71c80aac84f19c0b72ff387417d0bca6a4ded353
prakashRanjan19 <prakash.r19500@gmail.com>
2020-04-19 00:07:30 +0530

    master

--- app/src/main/java/com/introid/cheatchat/NewMessagesActivity.kt
+++ app/src/main/java/com/introid/cheatchat/NewMessagesActivity.kt
@@ -2,6 +2,11 @@ package com.introid.cheatchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_messages.*
 
 class NewMessagesActivity : AppCompatActivity() {
 
@@ -10,5 +15,18 @@ class NewMessagesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_messages)
 
         supportActionBar?.title = "Select User"
+
+        val adapter= GroupAdapter< GroupieViewHolder>()
+
+        rv_newMessage.adapter= adapter
     }
+}
+class UserItem : Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+    override fun getLayout(): Int {
+       return R.layout.item_new_user
+    }
+
 }
